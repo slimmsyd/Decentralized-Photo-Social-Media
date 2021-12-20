de831d5567617a90aa0090dabcc773982f221e3a
Sydney Sanders <ssanderss444@gmail.com>
2021-12-20 18:47:47 -0500

    Updates

---
+++ precache-manifest.95a9863fc32e464873c57f0946e57950.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f568c1a9f710195d5cfc1358bff436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3b4d57f6908574c42b",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "c1191592b68a064cab48",
+    "url": "/static/css/main.3fbfb760.chunk.css"
+  },
+  {
+    "revision": "5f3b4d57f6908574c42b",
+    "url": "/static/js/2.7e4d34c3.chunk.js"
+  },
+  {
+    "revision": "c1191592b68a064cab48",
+    "url": "/static/js/main.35235f34.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
